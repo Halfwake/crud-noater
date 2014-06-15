e2653e7624fc59b6b5bb789cc8cd3e33e7793eff
unknown <drewdudash@gmail.com>
2014-06-15 15:03:28 -0400

    remove old self from args from oop design

--- noater_db.py
+++ noater_db.py
@@ -4,18 +4,18 @@ def build_crud(host, port, db_name, collection_name):
   client = pymongo.MongoClient(host, port)
   collection = client[db_name][collection_name]
 
-  def create(self, data):
+  def create(data):
     for name, noat in data.items():
       collection.update({'name' : name}, {'$set' : noat}, upset = True)
 
-  def read(self, data):
+  def read(data):
     return [collection.find_one({'name' : name }) for name in data]
 
-  def update(self, data):
+  def update(data):
     for name, noat in data.items():
       collection.update({'name' : name}, {'$set' : noat}, upsert = False)
 
-  def delete(self, data):
+  def delete(data):
     for name in data:
       collection.remove ({'name' : name})
 
